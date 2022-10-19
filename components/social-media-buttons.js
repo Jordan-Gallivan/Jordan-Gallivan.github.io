3836261dbc863cb0abe7a0b78c6148403e15ab6f
Jordan-Gallivan <115050475+Jordan-Gallivan@users.noreply.github.com>
2022-10-18 20:43:34 -0400

    Add files via upload

---
+++ components/social-media-buttons.js
@@ -0,0 +1,47 @@
+class SocialMediaButtons extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+connectedCallback() {
+  this.innerHTML = `
+  <style>
+  .fa {
+    padding: 10px;
+    font-size: 0.9375em;
+    width: 15px;
+    text-align: center;
+    margin-left: 10px;
+    text-decoration: none;
+    border-radius: 50%;
+  }
+    /* git-hub */
+    .fa-github {
+        background: #3B5998;
+        color: #3B5998;
+      }
+    
+      /* linkedin */
+      .fa-linkedin {
+        background: #3B5998;
+        color: #3B5998;
+      }
+      /* feed */
+      .fa-feed {
+        background: #3B5998;
+        color: #3B5998;
+      }
+      .fa:hover {
+        opacity: 0.7;
+      }
+  </style>
+  
+  <a href="https://github.com/Jordan-Gallivan" class="fa fa-github" target="_blank"></a>
+  <a href="https://www.linkedin.com/in/jordan-gallivan/" class="fa fa-linkedin" target="_blank"></a>
+  <a href="feed" class="fa fa-feed" target="_blank"></a>
+
+  `;
+}
+}
+
+customElements.define('social-media-buttons', SocialMediaButtons);
---
+++ index.js
@@ -0,0 +1,32 @@
+// class Header extends HTMLElement {
+//   constructor() {
+//     super()
+//   }
+
+// }
+// customElements.define('Header',Header);
+
+
+
+
+function showMenu(x) {
+  x.classList.toggle("change");
+}
+
+
+document.addEventListener('click', e => {
+  const isDropdownButton = e.target.matches("[data-dropdown-button]")
+
+  if (!isDropdownButton && e.target.closest('[data-dropdown]') !=null) return
+
+  let currentDropdown
+  if (isDropdownButton) {
+    currentDropdown = e.target.closest('[data-dropdown]')
+    currentDropdown.classList.toggle("active")
+  }
+
+  document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+    if (dropdown === currentDropdown) return
+    dropdown.classList.remove('active')
+  })
+})
