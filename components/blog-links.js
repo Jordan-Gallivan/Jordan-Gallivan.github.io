ddf589b80dca9af9d512e8b1f05ee316080b4096
Jordan-Gallivan <115050475+Jordan-Gallivan@users.noreply.github.com>
2022-10-25 20:12:50 -0400

    Update blog-links.js

--- components/blog-links.js
+++ components/blog-links.js
@@ -1,7 +1,7 @@
 let blogLinks = [];
 
 // Post Object Location
-const blogObject = {Now_for_Some_Automation:"24 Oct 2022",How_did_I_get_here: "18 Oct 2022",
+const blogObject = {Custom_HTML_Elements:"25 Oct 2022",Now_for_Some_Automation:"24 Oct 2022",How_did_I_get_here: "18 Oct 2022",
 It_cant_be_that_hard: "18 Oct 2022"};
 
 for(title in blogObject) {
